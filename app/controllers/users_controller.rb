8f43622eeb186270ceaa0e711fd42ae872b29812
Julia Kimmig <juliakimmig@gmail.com>
2019-04-11 14:34:17 +0800

    create class room

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
     @user = current_user
     @teacher = current_user.teachers.first
     @professional = current_user.professionals.first
+    @class_room = ClassRoom.new(teacher: @teacher)
   end
+
 end
