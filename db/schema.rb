0a3ca7e387b08c51fc685f83069bf3f3d1b2dc1b
Grant Ye <yeyichen@hotmail.com>
2019-04-12 12:15:34 +0800

    login logout fixed

--- db/schema.rb
+++ db/schema.rb
@@ -43,12 +43,15 @@ ActiveRecord::Schema.define(version: 2019_04_10_075901) do
     t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message"
     t.bigint "class_room_id"
     t.index ["class_room_id"], name: "index_lectures_on_class_room_id"
     t.index ["professional_id"], name: "index_lectures_on_professional_id"
   end
 
   create_table "professionals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "company"
     t.string "title"
     t.string "description"
@@ -82,12 +85,10 @@ ActiveRecord::Schema.define(version: 2019_04_10_075901) do
   end
 
   create_table "teachers", force: :cascade do |t|
-    t.string "school"
-    t.string "title"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_teachers_on_user_id"
+    t.string "school"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
