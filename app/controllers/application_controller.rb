99c81852f41c473057387f08de3cbe8123619813
frank zhu <qf.zhu.frank@gmail.com>
2019-04-22 17:20:56 +0800

    fixing sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    current_user.professionals.empty? ? user_path(current_user.id) : p_dashboard_path(current_user.id)
+    if current_user.professionals.empty? && current_user.teachers.empty?
+      signup_form_professional_path
+    else
+      current_user.professionals.empty? ? user_path(current_user.id) : p_dashboard_path(current_user.id)
+    end
   end
 end
